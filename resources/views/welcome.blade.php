2702da15a34a9571338db0e2e32b5ba4d1897703
koykoy027 <villanuevajoshua27@gmail.com>
2024-07-22 11:31:06 +0800

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Axios Example</title>
+    <title>GrandShipper Test</title>
     <style>
         form {
             margin: 5%;
