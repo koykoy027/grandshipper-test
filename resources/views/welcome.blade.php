ffa872a1cab2f6deb11eb3fc326e204a3c6248e8
koykoy027 <villanuevajoshua27@gmail.com>
2024-07-24 00:26:26 +0800

    test commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GrandShipper Test</title>
+    <title>Test commit</title>
     <style>
         form {
             margin: 5%;
